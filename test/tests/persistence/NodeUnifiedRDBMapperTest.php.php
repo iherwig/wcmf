776bf08cf35b6dcbdf719685ec94cfa055616eb1
iherwig <iherwig@6ea2c64b-b214-0410-812b-cca8261b5cfa>
2011-02-15 00:37:39 +0000

    adjusted tests to latest changes
    git-svn-id: https://wcmf.svn.sourceforge.net/svnroot/wcmf/branches/new_roles/@1285 6ea2c64b-b214-0410-812b-cca8261b5cfa

--- test/tests/persistence/NodeUnifiedRDBMapperTest.php.php
+++ test/tests/persistence/NodeUnifiedRDBMapperTest.php.php
@@ -6,95 +6,85 @@ require_once(WCMF_BASE . "test/lib/WCMFTestCase.php");
 class NodeUnifiedRDBMapperTest extends WCMFTestCase {
 
   public function testSelect() {
-    $mapper = new PageRDBMapper(array('dbType' => 'mysql', 'dbHostName' => 'localhost', 
-        'dbUserName' => 'root', 'dbPassword' => '', 'dbName' => 'wcmf', 'dbPrefix' => ''));
+    $mapper = new PageRDBMapper(array('dbType' => 'mysql', 'dbHostName' => 'localhost',
+        'dbUserName' => 'root', 'dbPassword' => '', 'dbName' => 'wcmf_newroles', 'dbPrefix' => ''));
 
     // condition
-    $sql = $mapper->getSelectSQL($mapper->quoteIdentifier('Page').".".$mapper->quoteIdentifier('name')." = 'Page 1'");    
-    $expected = "SELECT `Page`.`id` AS `id`, `Page`.`fk_page_id` AS `fk_page_id`, `Page`.`fk_author_id` AS `fk_author_id`, ".
-      "`Page`.`name` AS `name`, `Page`.`created` AS `created`, `Page`.`creator` AS `creator`, `Page`.`modified` AS `modified`, ".
-      "`Page`.`last_editor` AS `last_editor`, `Page`.`sortkey` AS `sortkey`, 'Author' AS `ptype0`, 'Author' AS `prole0`, ".
-      "`Page`.`fk_author_id` AS `pid0`, 'Page' AS `ptype1`, 'ParentPage' AS `prole1`, `Page`.`fk_page_id` AS `pid1` ".
-      "FROM `Page` WHERE `Page`.`name` = 'Page 1' ORDER BY `Page`.`sortkey`;";
-    $this->assertTrue($sql === $expected);
+    $sql = $mapper->getSelectSQL($mapper->quoteIdentifier('Page').".".$mapper->quoteIdentifier('name')." = 'Page 1'");
+    $expected = "SELECT `Page`.`id`, `Page`.`fk_page_id`, `Page`.`fk_author_id`, `Page`.`name`, `Page`.`created`, ".
+      "`Page`.`creator`, `Page`.`modified`, `Page`.`last_editor`, `Page`.`sortkey`, `Author`.`name` AS `author_name` ".
+      "FROM `Page` LEFT JOIN `Author` ON `Page`.`fk_author_id`=`Author`.`id` WHERE (`Page`.`name` = 'Page 1') ".
+      "ORDER BY `Page`.`sortkey` ASC";
+    $this->assertTrue(str_replace("\n", "", $sql) === $expected);
 
     // alias
-    $sql = $mapper->getSelectSQL($mapper->quoteIdentifier('Page').".".$mapper->quoteIdentifier('name')." = 'Page 1'", 
-            "PageAlias");    
-    $expected = "SELECT `PageAlias`.`id` AS `id`, `PageAlias`.`fk_page_id` AS `fk_page_id`, `PageAlias`.`fk_author_id` AS `fk_author_id`, ".
-      "`PageAlias`.`name` AS `name`, `PageAlias`.`created` AS `created`, `PageAlias`.`creator` AS `creator`, `PageAlias`.`modified` AS `modified`, ".
-      "`PageAlias`.`last_editor` AS `last_editor`, `PageAlias`.`sortkey` AS `sortkey`, 'Author' AS `ptype0`, 'Author' AS `prole0`, ".
-      "`PageAlias`.`fk_author_id` AS `pid0`, 'Page' AS `ptype1`, 'ParentPage' AS `prole1`, `PageAlias`.`fk_page_id` AS `pid1` ".
-      "FROM `Page` AS `PageAlias` WHERE `PageAlias`.`name` = 'Page 1' ORDER BY `PageAlias`.`sortkey`;";
-    $this->assertTrue($sql === $expected);
+    $sql = $mapper->getSelectSQL($mapper->quoteIdentifier('Page').".".$mapper->quoteIdentifier('name')." = 'Page 1'",
+            "PageAlias");
+    $expected = "SELECT `PageAlias`.`id`, `PageAlias`.`fk_page_id`, `PageAlias`.`fk_author_id`, `PageAlias`.`name`, ".
+      "`PageAlias`.`created`, `PageAlias`.`creator`, `PageAlias`.`modified`, `PageAlias`.`last_editor`, ".
+      "`PageAlias`.`sortkey`, `Author`.`name` AS `author_name` FROM `Page` AS `PageAlias` ".
+      "LEFT JOIN `Author` ON `PageAlias`.`fk_author_id`=`Author`.`id` WHERE (`PageAlias`.`name` = 'Page 1') ".
+      "ORDER BY `PageAlias`.`sortkey` ASC";
+    $this->assertTrue(str_replace("\n", "", $sql) === $expected);
 
     // order
-    $sql = $mapper->getSelectSQL($mapper->quoteIdentifier('Page').".".$mapper->quoteIdentifier('name')." = 'Page 1'", 
-            null, $mapper->quoteIdentifier('Page').".".$mapper->quoteIdentifier('name')." ASC");
-    $expected = "SELECT `Page`.`id` AS `id`, `Page`.`fk_page_id` AS `fk_page_id`, `Page`.`fk_author_id` AS `fk_author_id`, ".
-      "`Page`.`name` AS `name`, `Page`.`created` AS `created`, `Page`.`creator` AS `creator`, `Page`.`modified` AS `modified`, ".
-      "`Page`.`last_editor` AS `last_editor`, `Page`.`sortkey` AS `sortkey`, 'Author' AS `ptype0`, 'Author' AS `prole0`, ".
-      "`Page`.`fk_author_id` AS `pid0`, 'Page' AS `ptype1`, 'ParentPage' AS `prole1`, `Page`.`fk_page_id` AS `pid1` ".
-      "FROM `Page` WHERE `Page`.`name` = 'Page 1' ORDER BY `Page`.`name` ASC;";
-    $this->assertTrue($sql === $expected);
+    $sql = $mapper->getSelectSQL($mapper->quoteIdentifier('Page').".".$mapper->quoteIdentifier('name')." = 'Page 1'",
+            null, "Page.name ASC");
+    $expected = "SELECT `Page`.`id`, `Page`.`fk_page_id`, `Page`.`fk_author_id`, `Page`.`name`, `Page`.`created`, ".
+      "`Page`.`creator`, `Page`.`modified`, `Page`.`last_editor`, `Page`.`sortkey`, `Author`.`name` AS `author_name` ".
+      "FROM `Page` LEFT JOIN `Author` ON `Page`.`fk_author_id`=`Author`.`id` WHERE (`Page`.`name` = 'Page 1') ".
+      "ORDER BY `Page`.`name` ASC";
+    $this->assertTrue(str_replace("\n", "", $sql) === $expected);
 
-    // attribs
-    $sql = $mapper->getSelectSQL($mapper->quoteIdentifier('Page').".".$mapper->quoteIdentifier('name')." = 'Page 1'", 
+    // attribs 1
+    $sql = $mapper->getSelectSQL($mapper->quoteIdentifier('Page').".".$mapper->quoteIdentifier('name')." = 'Page 1'",
             null, null, array('id', 'name'));
-    $expected = "SELECT `Page`.`id` AS `id`, `Page`.`name` AS `name`, 'Author' AS `ptype0`, 'Author' AS `prole0`, ".
-      "`Page`.`fk_author_id` AS `pid0`, 'Page' AS `ptype1`, 'ParentPage' AS `prole1`, `Page`.`fk_page_id` AS `pid1` ".
-      "FROM `Page` WHERE `Page`.`name` = 'Page 1' ORDER BY `Page`.`sortkey`;";
-    $this->assertTrue($sql === $expected);
+    $expected = "SELECT `Page`.`id`, `Page`.`name` FROM `Page` WHERE (`Page`.`name` = 'Page 1') ".
+      "ORDER BY `Page`.`sortkey` ASC";
+    $this->assertTrue(str_replace("\n", "", $sql) === $expected);
+
+    // attribs 2
+    $sql = $mapper->getSelectSQL($mapper->quoteIdentifier('Page').".".$mapper->quoteIdentifier('name')." = 'Page 1'",
+            null, null, array('id', 'name', 'author_name'));
+    $expected = "SELECT `Page`.`id`, `Page`.`name`, `Author`.`name` AS `author_name` FROM `Page` ".
+      "LEFT JOIN `Author` ON `Page`.`fk_author_id`=`Author`.`id` WHERE (`Page`.`name` = 'Page 1') ".
+      "ORDER BY `Page`.`sortkey` ASC";
+    $this->assertTrue(str_replace("\n", "", $sql) === $expected);
 
     // dbprefix
-    $mapper = new PageRDBMapper(array('dbType' => 'mysql', 'dbHostName' => 'localhost', 
-        'dbUserName' => 'root', 'dbPassword' => '', 'dbName' => 'wcmf', 'dbPrefix' => 'WCMF_'));
+    $mapper = new PageRDBMapper(array('dbType' => 'mysql', 'dbHostName' => 'localhost',
+        'dbUserName' => 'root', 'dbPassword' => '', 'dbName' => 'wcmf_newroles', 'dbPrefix' => 'WCMF_'));
 
     // condition
-    $sql = $mapper->getSelectSQL($mapper->quoteIdentifier('Page').".".$mapper->quoteIdentifier('name')." = 'Page 1'");    
-    $expected = "SELECT `WCMF_Page`.`id` AS `id`, `WCMF_Page`.`fk_page_id` AS `fk_page_id`, `WCMF_Page`.`fk_author_id` AS `fk_author_id`, ".
-      "`WCMF_Page`.`name` AS `name`, `WCMF_Page`.`created` AS `created`, `WCMF_Page`.`creator` AS `creator`, `WCMF_Page`.`modified` AS `modified`, ".
-      "`WCMF_Page`.`last_editor` AS `last_editor`, `WCMF_Page`.`sortkey` AS `sortkey`, 'Author' AS `ptype0`, 'Author' AS `prole0`, ".
-      "`WCMF_Page`.`fk_author_id` AS `pid0`, 'Page' AS 'ptype1', 'ParentPage' AS `prole1`, `WCMF_Page`.`fk_page_id` AS `pid1` ".
-      "FROM `WCMF_Page` WHERE `WCMF_Page`.`name` = 'Page 1' ORDER BY `WCMF_Page`.`sortkey`;";
-    $this->assertTrue($sql === $expected);
-
-    
-    $mapper = new UserRDBRDBMapper(array('dbType' => 'mysql', 'dbHostName' => 'localhost', 
-        'dbUserName' => 'root', 'dbPassword' => '', 'dbName' => 'wcmf', 'dbPrefix' => ''));
-
-    // condition
-    $sql = $mapper->getSelectSQL($mapper->quoteIdentifier('User').".".$mapper->quoteIdentifier('name')." = 'Page 1'");    
-    $expected = "SELECT `WCMF_Page`.`id` AS `id`, `WCMF_Page`.`fk_page_id` AS `fk_page_id`, `WCMF_Page`.`fk_author_id` AS `fk_author_id`, ".
-      "`WCMF_Page`.`name` AS `name`, `WCMF_Page`.`created` AS `created`, `WCMF_Page`.`creator` AS `creator`, `WCMF_Page`.`modified` AS `modified`, ".
-      "`WCMF_Page`.`last_editor` AS `last_editor`, `WCMF_Page`.`sortkey` AS `sortkey`, `Author` AS `ptype0`, `Author` AS `prole0`, ".
-      "`WCMF_Page`.`fk_author_id` AS `pid0`, `WCMF_Page` AS `ptype1`, `ParentPage` AS `prole1`, `WCMF_Page`.`fk_page_id` AS `pid1` ".
-      "FROM `WCMF_Page` WHERE `WCMF_Page`.`name` = 'Page 1' ORDER BY `WCMF_Page`.`sortkey`;";
-    $this->assertTrue($sql === $expected);
-
+    $sql = $mapper->getSelectSQL($mapper->quoteIdentifier('Page').".".$mapper->quoteIdentifier('name')." = 'Page 1'");
+    $expected = "SELECT `WCMF_Page`.`id`, `WCMF_Page`.`fk_page_id`, `WCMF_Page`.`fk_author_id`, `WCMF_Page`.`name`, ".
+      "`WCMF_Page`.`created`, `WCMF_Page`.`creator`, `WCMF_Page`.`modified`, `WCMF_Page`.`last_editor`, `WCMF_Page`.`sortkey`, ".
+      "`Author`.`name` AS `author_name` FROM `WCMF_Page` LEFT JOIN `Author` ON `WCMF_Page`.`fk_author_id`=`Author`.`id` ".
+      "WHERE (`WCMF_Page`.`name` = 'Page 1') ORDER BY `WCMF_Page`.`sortkey` ASC";
+    $this->assertTrue(str_replace("\n", "", $sql) === $expected);
   }
 
   public function testRelation() {
     // parent
-    
+
     // child
-    
+
     // many to many
-    
+
   }
-  
+
   public function testDisassociate() {
   }
 
   public function testInsert() {
   }
-  
+
   public function testUpdate() {
   }
 
   public function testDelete() {
   }
-  
+
   public function testReference() {
   }
 }
--- test/tests/persistence/PersistentObjectTest.php
+++ test/tests/persistence/PersistentObjectTest.php
@@ -4,21 +4,41 @@ require_once(WCMF_BASE."test/lib/WCMFTestCase.php");
 
 class PersistentObjectTest extends WCMFTestCase
 {
-  public function _testLoadSingle()
+  public function testLoadSingle()
   {
     $this->runAnonymous(true);
     $persistenceFacade = PersistenceFacade::getInstance();
 
     $page = $persistenceFacade->load(new ObjectId('Page', array(1)), BUILDDEPTH_SINGLE);
+    $page->loadChildren('Document', 1);
+    /*
     foreach($page->getValueNames() as $name) {
       $value = $page->getValue($name);
       echo $name.": ".$value."(".sizeof($value).")\n";
     }
-
+    */
     $this->runAnonymous(false);
   }
 
-  public function testLoadMany()
+  function _testCreateRandom() {
+    $this->runAnonymous(true);
+    $alphanum = "abcdefghijkmnpqrstuvwxyz23456789";
+    $pf = PersistenceFacade::getInstance();
+    for ($i=0; $i<100; $i++) {
+      $doc = $pf->create('Page', BUILDDEPTH_SINGLE);
+      $inc = 1;
+      while ($inc < 15){
+        $alphanum = $alphanum.'abcdefghijkmnpqrstuvwxyz23456789';
+        $inc++;
+      }
+      $title = substr(str_shuffle($alphanum), 0, 15);
+      $doc->setName(ucfirst($title));
+      $doc->save();
+    }
+    $this->runAnonymous(false);
+  }
+
+  public function _testLoadMany()
   {
     $this->runAnonymous(true);
     $persistenceFacade = PersistenceFacade::getInstance();
