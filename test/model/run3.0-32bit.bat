a089b5db75fadc5a66bd5494940d9bf478e9e3af
iherwig <iherwig@users.sourceforge.net>
2012-04-24 02:01:47 +0200

    added test model

---
+++ test/model/run3.0-32bit.bat
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\Java\jre6\bin\java" -Djava.library.path=../../../generator/lib/ -jar ../../../generator/wCMFGenerator.jar ../../../generator/workflow/workflow.oaw -basePath=../../../generator/ -propertyFile=workflow.properties
---
+++ test/model/run3.0.bat
@@ -0,0 +1,1 @@
+java -Djava.library.path=../../../generator/lib/ -jar ../../../generator/wCMFGenerator.jar ../../../generator/workflow/workflow.oaw -basePath=../../../generator/ -propertyFile=workflow.properties
