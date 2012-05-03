663901726b043098041c171ee598ca9e0685652c
iherwig <iherwig@users.sourceforge.net>
2012-05-03 09:29:27 +0200

    switched test model to chronos profile

--- test/model/run3.0.bat
+++ test/model/run3.0.bat
@@ -1,1 +1,1 @@
-java -Djava.library.path=../../../generator/lib/ -jar ../../../generator/wCMFGenerator.jar ../../../generator/workflow/workflow.oaw -basePath=../../../generator/ -propertyFile=workflow.properties
+java -Djava.library.path=../../../generator/lib/ -jar ../../../generator/ChronosGenerator.jar ../../../generator/workflow/workflow.oaw -basePath=../../../generator/ -propertyFile=workflow.properties
