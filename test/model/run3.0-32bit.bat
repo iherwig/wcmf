79d73aec3cb39a404b8f31906b138f2bee41c157
iherwig <iherwig@users.sourceforge.net>
2012-06-27 01:45:31 +0200

    restructured test model directories, updated test model

--- test/model/run3.0-32bit.bat
+++
@@ -1,1 +0,0 @@
-"C:\Program Files (x86)\Java\jre6\bin\java" -Djava.library.path=../../../generator/lib/ -jar ../../../generator/ChronosGenerator.jar ../../../generator/cartridge/Wcmf/workflow/wcmf.oaw -basePath=../../../generator/ -propertyFile=workflow.properties
