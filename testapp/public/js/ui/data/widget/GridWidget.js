bdce7e8a82c1ecfea238426773e870be9b72746c
iherwig <iherwig@users.sourceforge.net>
2013-03-26 17:13:45 +0100

    changed submodule urls, added missing files

---
+++ testapp/public/js/ui/data/widget/GridWidget.js
@@ -0,0 +1,160 @@
+/*jshint strict:false */
+
+define([
+    "dojo/_base/declare",
+    "dojo/_base/lang",
+    "dijit/_WidgetBase",
+    "dijit/_TemplatedMixin",
+    "dojomat/_StateAware",
+    "dgrid/OnDemandGrid",
+    "dgrid/Selection",
+    "dgrid/editor",
+    "dojo/store/Observable",
+    "dojo/dom-construct",
+    "dojo/dom-style",
+    "dojo/dom-attr",
+    "dojo/aspect",
+    "dojo/topic",
+    "dojo/on",
+    "dojo/when",
+    "dojo/promise/all",
+    "dojo/text!./template/GridWidget.html"
+], function (
+    declare,
+    lang,
+    _WidgetBase,
+    _TemplatedMixin,
+    _StateAware,
+    OnDemandGrid,
+    Selection,
+    editor,
+    Observable,
+    domConstruct,
+    domStyle,
+    domAttr,
+    aspect,
+    topic,
+    on,
+    when,
+    all,
+    template
+) {
+    return declare([_WidgetBase, _TemplatedMixin, _StateAware], {
+
+        router: null,
+        store: null,
+        templateString: template,
+        gridWidget: null,
+
+        constructor: function (params) {
+            this.request = params.request;
+            this.router = params.router;
+            this.store = params.store;
+        },
+
+        postCreate: function () {
+            this.inherited(arguments);
+            this.store = Observable(this.store);
+            this.gridWidget = this.buildGrid();
+            this.gridWidget.set("store", this.store); // set store and run query
+        },
+
+        startup: function () {
+        },
+
+        buildGrid: function () {
+            var columns = {
+                    selector: {
+                        label: ' ',
+                        sortable: false
+                    },
+
+                    title: {
+                        label: "Title",
+                        field: "title",
+                        sortable: true
+                    },
+
+                    date: {
+                        label: "Date",
+                        field: "date",
+                        sortable: true
+                    }
+                },
+                gridWidget = new (declare([OnDemandGrid, Selection]))({
+                    getBeforePut: true,
+                    columns: columns,
+                    selectionMode: 'none', // we'll do programmatic selection with a checkbox
+                    //query: { find: 'xx' },
+                    queryOptions: { sort: [{ attribute: 'title', descending: false }] },
+                    loadingMessage: "Loading",
+                    noDataMessage: "No data"
+                }, this.gridNode),
+                countSelectedItems = function () {
+                    var count = 0, i;
+
+                    for (i in gridWidget.selection) {
+                        if (gridWidget.selection.hasOwnProperty(i)) {
+                            count = count + 1;
+                        }
+                    }
+
+                    return count;
+                }
+            ;
+
+            if (this.request.getQueryParam('find')) {
+                gridWidget.set('query', {
+                    title: this.request.getQueryParam('find')
+                });
+            }
+
+            gridWidget.on("dgrid-error", function (evt) {
+                topic.publish('ui/data/widget/GridWidget/unknown-error', {
+                    notification: {
+                        message: "Unknown Error",
+                        type: 'error'
+                    }
+                });
+            });
+
+            // click on title column header
+            gridWidget.on(".dgrid-header .dgrid-column-title:click", lang.hitch(this, function (evt) {
+                //console.dir(gridWidget.cell(evt))
+            }));
+
+            // click on title data cell
+            gridWidget.on(".dgrid-row .dgrid-column-title:click", lang.hitch(this, function (evt) {
+                //console.dir(gridWidget.row(evt));
+            }));
+
+            // row selected
+            gridWidget.on("dgrid-select", lang.hitch(this, function (evt) {
+                //console.dir(evt.gridWidget.selection);
+            }));
+
+            // row deselected
+            gridWidget.on("dgrid-deselect", lang.hitch(this, function (evt) {
+                //console.dir(evt.gridWidget.selection);
+            }));
+
+            gridWidget.on("dgrid-datachange", lang.hitch(this, function (evt) {
+            }));
+
+            //var loadHandle = aspect.after(gridWidget, '_trackError', lang.hitch(this, function (promiseOrResult) {
+            var loadHandle = aspect.after(this.store, 'query', lang.hitch(this, function (promiseOrResult) {
+                when(
+                    promiseOrResult,
+                    lang.hitch(this, function (data) {
+                        if (data.length) {
+                            loadHandle.remove(); // only run once and remove when data has loaded
+                        }
+                    })
+                );
+                return promiseOrResult;
+            }));
+
+            return gridWidget;
+        }
+    });
+});
