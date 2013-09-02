e635eacc1b29b4c5c2a5b674d11325f4bf52ef3c
iherwig <iherwig@users.sourceforge.net>
2013-09-03 00:55:40 +0200

    cleanup build, removed RewriteBase

--- testapp/_build/build.bat
+++
@@ -1,8 +0,0 @@
-@rem
-@rem Run dojo build
-@rem
-@rem Note: --profile must be relative to dojo/util/buildscripts/build.sh
-@rem
-
-cd ..\public\vendor\dojo\util\buildscripts\
-build.bat --profile ..\..\..\..\..\_build\app.profile.js
--- testapp/_build/build.sh
+++
@@ -1,8 +0,0 @@
-#
-# Run dojo build
-#
-# Note: --profile must be relative to dojo/util/buildscripts/build.sh
-#
-
-cd ../public/vendor/dojo/util/buildscripts/
-build.sh --profile ../../../../../_build/app.profile.js
