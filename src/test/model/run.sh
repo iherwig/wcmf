cf25f070dc3208d5b6a01ba26e58ef3bc4d49661
iherwig <iherwig@users.sourceforge.net>
2013-10-29 01:51:03 +0100

    integrated code generation into ant build script

--- src/test/model/run.sh
+++
@@ -1,8 +0,0 @@
-generatorBase=E:/Daten/web/olympos/generator/dist
-targetDir=E:/Daten/web/wcmf/src
-
-java -Djava.library.path=$generatorBase/lib \
-  -jar $generatorBase/ChronosGenerator.jar $generatorBase/cartridge/Wcmf/workflow/wcmf.oaw \
-  -basePath=$generatorBase/ \
-  -propertyFile=workflow.properties \
-  -targetDir=$targetDir
