478d7827ddb15fc98f1eea4da1092a134b8c6643
ingo <ingo@wemove.com>
2014-08-20 19:35:23 +0200

    documentation

--- docs/api-src/theme/js/application.js
+++ docs/api-src/theme/js/application.js
@@ -4,12 +4,7 @@ $(function() {
   // create toc
   if ($(".has-toc").length > 0) {
 
-//    <div class="col-sm-offset-1 col-sm-10 panel panel-default">
-
-//    <div class="col-sm-1 hidden-xs"><div id="toc"></div></div>
-//    <div class="col-sm-offset-1 col-sm-10 panel panel-default">
-
-    $('<div class="col-sm-1 hidden-xs"><div id="toc"></div></div>').insertBefore( ".panel-default" );
+    $('<div class="col-md-1 visible-lg"><div id="toc"></div></div>').insertBefore( ".panel-default" );
 
     var toc = $("#toc").tocify({
       selectors: "h2,h3,h4,h5",
