5cb9acd766023596539bf02215cc6db43820a85e
iherwig <iherwig@users.sourceforge.net>
2014-10-18 23:46:20 +0200

    docs: styled search popup

--- docs/api-src/theme/js/application.js
+++ docs/api-src/theme/js/application.js
@@ -16,4 +16,31 @@ $(function() {
   // search
   searchBox.OnSelectItem(0);
   $('#MSearchSelectWindow').hide();
+  $('#MSearchBox').css("margin-top", 8);
+
+  var sResultWin = $('#MSearchResultsWindow');
+  sResultWin.addClass('dropdown-menu');
+
+  var iframe = $('#MSearchResults');
+  iframe.on( "load", function() {
+    iframe.addClass('embed-responsive-item');
+    var head = iframe.contents().find('head');
+    var body = iframe.contents().find('body');
+    head.find('link').remove();
+    head.append($("<link/>", {
+      rel: "stylesheet",
+      href: "../theme/css/style.css",
+      type: "text/css"
+    }));
+    head.append($("<link/>", {
+      rel: "stylesheet",
+      href: "../theme/bootstrap3/css/bootstrap.min.css",
+      type: "text/css"
+    }));
+    body.css("padding", 10);
+
+    if (sResultWin.offset().left < 0) {
+      sResultWin.css("left", 0);
+    }
+  });
 });
