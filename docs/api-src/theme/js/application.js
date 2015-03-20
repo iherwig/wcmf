20c9d5d824e4836df17f95685ab5610e70e1b5d9
ingo <ingo@wemove.com>
2015-03-20 12:23:13 +0100

    documentation

--- docs/api-src/theme/js/application.js
+++ docs/api-src/theme/js/application.js
@@ -19,7 +19,7 @@ $(function() {
     var el, icon, id;
     el = $(el);
     id = el.find('a').first().attr('id');
-    icon = '<i class="fa fa-link"></i>';
+    icon = '<i class="fa fa-slack"></i>';
     if (id) {
       return el.prepend($("<a />").addClass("header-link").attr("href", "#" + id).html(icon));
     }
