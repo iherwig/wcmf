84480edff67429047a934865d505dfd9b24ebd31
ingo <ingo@wemove.com>
2015-08-11 16:19:32 +0200

    refactoring

--- src/wcmf/test/resources/app/src/lib/EventListener.php
+++ src/wcmf/test/resources/app/src/lib/EventListener.php
@@ -1,8 +1,9 @@
 <?php
 namespace app\src\lib;
 
-use wcmf\lib\core\ObjectFactory;
+use wcmf\lib\core\EventManager;
 use wcmf\lib\persistence\PersistenceEvent;
+use wcmf\lib\presentation\view\View;
 
 /**
  * EventListener
@@ -12,20 +13,25 @@ use wcmf\lib\persistence\PersistenceEvent;
 class EventListener {
 
   private $_eventManager = null;
+  private $_view = null;
 
   /**
    * Constructor
    */
-  public function __construct() {
-    $this->_eventManager = ObjectFactory::getInstance('eventManager');
-    $this->_eventManager->addListener(PersistenceEvent::NAME, array($this, 'persisted'));
+  public function __construct(EventManager $eventManager,
+          View $view) {
+    $this->_eventManager = $eventManager;
+    $this->_view = $view;
+    $this->_eventManager->addListener(PersistenceEvent::NAME,
+      array($this, 'persisted'));
   }
 
   /**
    * Destructor
    */
   public function __destruct() {
-    $this->_eventManager->removeListener(PersistenceEvent::NAME, array($this, 'persisted'));
+    $this->_eventManager->removeListener(PersistenceEvent::NAME,
+      array($this, 'persisted'));
   }
 
   /**
@@ -40,8 +46,7 @@ class EventListener {
    * Invalidate cached views on object change.
    */
   protected function invalidateCachedViews() {
-    $view = ObjectFactory::getInstance('view');
-    $view->clearCache();
+    $this->_view->clearCache();
   }
 }
 ?>
