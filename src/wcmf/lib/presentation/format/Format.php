42a35b8b9706f1f54b226a0be9d5db30be59184b
iherwig <ingo@wemove.com>
2016-10-07 22:46:32 +0200

    docs [ci skip]

--- src/wcmf/lib/presentation/format/Format.php
+++ src/wcmf/lib/presentation/format/Format.php
@@ -16,8 +16,9 @@ use wcmf\lib\presentation\Response;
 /**
  * Format defines the interface for all format classes. Format instances
  * are used to map external data representations like JSON, XML/SOAP or HTML
- * to internal ones. All data values are supposed to be scalar or array values
- * except for Nodes, for which each external representation defines a special notation.
+ * to internal ones and vice versa. All data values are supposed to be scalar or
+ * array values except for wcmf::lib::model::Node instances, for which each external
+ * representation defines a special notation.
  *
  * @author ingo herwig <ingo@wemove.com>
  */
