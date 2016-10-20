bddaeed2286b5fd819ae1531e37d7653d32e56d7
iherwig <ingo@wemove.com>
2016-10-21 00:33:10 +0200

    added application secret, removed auth token from session

--- src/wcmf/lib/core/impl/AuthTokenSession.php
+++ src/wcmf/lib/core/impl/AuthTokenSession.php
@@ -19,7 +19,7 @@ use wcmf\lib\util\URIUtil;
 
 /**
  * AuthTokenSession is a DefaultSession, but additionally requires
- * clients to send a token in the X-Auth-Token request header.
+ * clients to send a token in the X-Auth-Token request header (Double Submit Cookie).
  * The token is created, when the authenticated user is associated
  * with the session and send to the client in a cookie named
  * <em>application-title</em>-token.
@@ -44,7 +44,7 @@ class AuthTokenSession extends DefaultSession implements Session {
     // check for valid auth-token in request
     $this->tokenName = $this->getCookiePrefix().'-token';
     $this->isTokenValid = $request->hasHeader(self::TOKEN_HEADER) &&
-      $request->getHeader(self::TOKEN_HEADER) == $this->get($this->tokenName);
+      $request->getHeader(self::TOKEN_HEADER) == $_COOKIE[$this->tokenName];
   }
 
   /**
@@ -57,7 +57,6 @@ class AuthTokenSession extends DefaultSession implements Session {
     if ($login !== AnonymousUser::USER_GROUP_NAME) {
       // generate a token, set it in the session and send it to the client
       $token = base64_encode(openssl_random_pseudo_bytes(32));
-      $this->set($this->tokenName, $token);
       $this->isTokenValid = true;
       // NOTE: prevent "headers already sent" errors in phpunit tests
       if (!headers_sent()) {
