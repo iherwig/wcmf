5bb8cb10dcdf195aa1da7411869c1cba9195f097
iherwig <ingo@wemove.com>
2016-11-28 14:02:48 +0100

    fixing tests

--- src/wcmf/test/tests/service/SoapTest.php
+++ src/wcmf/test/tests/service/SoapTest.php
@@ -79,13 +79,8 @@ class SoapTest extends DatabaseTestCase {
   }
 
   public function testList() {
-    $wsdlUrl = self::getEndPoint().'?wsdl';
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $wsdlUrl);
-    curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
-    $wsdl = curl_exec($ch);
-    curl_close($ch);
-    echo "!".$wsdl;
+    $wsdl = file_get_contents('http://localhost:8500/soap?wsdl');
+    echo $wsdl;
 
     $options = array('trace' => 1, 'exceptions' => 0);
     $client = new SoapClient(self::getEndPoint().'?wsdl', 'admin', 'admin', $options);
