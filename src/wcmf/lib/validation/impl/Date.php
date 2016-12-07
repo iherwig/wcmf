d3341f563f4ec647a8f9331a68ee451063d7c4be
iherwig <ingo@wemove.com>
2016-12-07 19:46:14 +0100

    added Date, Required validation types

---
+++ src/wcmf/lib/validation/impl/Date.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * wCMF - wemove Content Management Framework
+ * Copyright (C) 2005-2016 wemove digital solutions GmbH
+ *
+ * Licensed under the terms of the MIT License.
+ *
+ * See the LICENSE file distributed with this work for
+ * additional information.
+ */
+namespace wcmf\lib\validation\impl;
+
+use wcmf\lib\i18n\Message;
+use wcmf\lib\validation\ValidateType;
+
+/**
+ * Date validates against the specified date format.
+ *
+ * Configuration examples:
+ * @code
+ * // validate against default format (Y-m-d)
+ * date
+ *
+ * // validate against j-M-Y format (e.g. 15-Feb-2009)
+ * date:{"format":"j-M-Y"}
+ * @endcode
+ *
+ * @author ingo herwig <ingo@wemove.com>
+ */
+class Date implements ValidateType {
+
+  const DEFAULT_FORMAT = 'Y-m-d';
+
+  /**
+   * @see ValidateType::validate
+   * $options is an associative array with keys 'format' (optional)
+   */
+  public function validate($value, Message $message, $options=null) {
+    $format = isset($options['format']) ? $options['format'] : self::DEFAULT_FORMAT;
+    return strlen($value) === 0 || (\DateTime::createFromFormat($format, $value) !== false);
+  }
+}
+?>
---
+++ src/wcmf/lib/validation/impl/Required.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * wCMF - wemove Content Management Framework
+ * Copyright (C) 2005-2016 wemove digital solutions GmbH
+ *
+ * Licensed under the terms of the MIT License.
+ *
+ * See the LICENSE file distributed with this work for
+ * additional information.
+ */
+namespace wcmf\lib\validation\impl;
+
+use wcmf\lib\i18n\Message;
+use wcmf\lib\validation\ValidateType;
+
+/**
+ * Required checks if the value is not empty.
+ *
+ * Configuration examples:
+ * @code
+ * required
+ * @endcode
+ *
+ * @author ingo herwig <ingo@wemove.com>
+ */
+class Required implements ValidateType {
+
+  /**
+   * @see ValidateType::validate
+   */
+  public function validate($value, Message $message, $options=null) {
+    return strlen($value) > 0;
+  }
+}
+?>
