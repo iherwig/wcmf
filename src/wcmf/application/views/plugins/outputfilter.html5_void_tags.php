e054bbe2b2e406a73157c84e623a9d6ef109c773
iherwig <ingo@wemove.com>
2017-01-04 03:03:07 +0100

    readded smarty filter

---
+++ src/wcmf/application/views/plugins/outputfilter.html5_void_tags.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * wCMF - wemove Content Management Framework
+ * Copyright (C) 2005-2016 wemove digital solutions GmbH
+ *
+ * Licensed under the terms of the MIT License.
+ *
+ * See the LICENSE file distributed with this work for
+ * additional information.
+ */
+
+/**
+ * Remove closing slashes from void tags according to html5.
+ *
+ * @param $output
+ * @param $template Smarty_Internal_Template
+ * @return String
+ */
+function smarty_outputfilter_html5_void_tags($output, Smarty_Internal_Template $template) {
+  // remove slashes from image tags
+  $output = preg_replace('/<img([^>]+) ?\/\>/i', "<img $1>", $output);
+  // replace brs
+  $output = preg_replace("/<br ?\/\>/i", "<br>", $output);
+
+  return $output;
+}
+?>
