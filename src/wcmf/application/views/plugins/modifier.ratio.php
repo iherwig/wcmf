2032984179c24f2cce27f8839fee34488df66e8f
iherwig <ingo@wemove.com>
2017-01-13 11:36:47 +0100

    added ratio smarty plugin

---
+++ src/wcmf/application/views/plugins/modifier.ratio.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * wCMF - wemove Content Management Framework
+ * Copyright (C) 2005-2017 wemove digital solutions GmbH
+ *
+ * Licensed under the terms of the MIT License.
+ *
+ * See the LICENSE file distributed with this work for
+ * additional information.
+ */
+
+/**
+ * Get the aspect ratio of the given image
+ *
+ * Example:
+ * @code
+ * {if $image|ratio < 1.78}
+ *    ...aspect ratio of image is smaller than 16:9, e.g 4:3...
+ * {else}
+ *    ...
+ * {/if}
+ * @endcode
+ *
+ * @param $image The image file
+ * @return Float
+ */
+function smarty_modifier_ratio($image) {
+  $size = getimagesize($image);
+  return $size[0]/$size[1];
+}
+?>
