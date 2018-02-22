5994f0af27d6a1ea5352b1723a0ab2d20c4f3db4
iherwig <ingo@wemove.com>
2018-02-22 19:44:45 +0100

    added smarty plugin for localizing dates

--- src/wcmf/application/views/plugins/function.daterange.php
+++ src/wcmf/application/views/plugins/function.daterange.php
@@ -11,17 +11,41 @@
 use wcmf\lib\core\ObjectFactory;
 
 /**
- * Format two dates avoiding duplicate years and months (e.g. 10. - 11.03.2016).
+ * Date names to be included by l10n tools
+ * - $message->getText("Monday")
+ * - $message->getText("Tuesday")
+ * - $message->getText("Wednesday")
+ * - $message->getText("Thursday")
+ * - $message->getText("Friday")
+ * - $message->getText("Saturday")
+ * - $message->getText("Sunday")
+ * - $message->getText("January")
+ * - $message->getText("February")
+ * - $message->getText("March")
+ * - $message->getText("April")
+ * - $message->getText("May")
+ * - $message->getText("June")
+ * - $message->getText("July")
+ * - $message->getText("August")
+ * - $message->getText("September")
+ * - $message->getText("October")
+ * - $message->getText("November")
+ * - $message->getText("December")
+ */
+
+/**
+ * Format one or two dates avoiding duplicate years and months (e.g. 10. - 11.03.2016).
  *
  * Example:
  * @code
- * {daterange from=$date->getValue('start_date') to=$date->getValue('end_date'}
+ * {daterange from=$date->getValue('start_date') to=$date->getValue('end_date') lang=$language}
  * @endcode
  *
  * @param $params Array with keys:
  *        - from: Start date in a format that can be parsed into a DateTime instance
  *        - to: End date in a format that can be parsed into a DateTime instance
- *        - lang: Language used to translate the date format (d.m.Y)
+ *        - lang: Language used to translate the date names
+ *        - formats: Array of date formats (day-month-year, day-month, day) to use (optional, default: ['d.m.Y', 'd.m.', 'd.'])
  * @param $template Smarty_Internal_Template
  * @return String
  */
@@ -47,20 +71,35 @@ function smarty_function_daterange(array $params, Smarty_Internal_Template $temp
   $sameMonth = $from->format('Y-m') == $to->format('Y-m');
   $sameYear = $from->format('Y') == $to->format('Y');
 
-  $message = ObjectFactory::getInstance('message');
-  $fullFormat = $message->getText('d.m.Y', null, $lang);
-  $monthFormat = $message->getText('d.m.', null, $lang);
-  $dayFormat = $message->getText('d.', null, $lang);
+  $formats = isset($params['formats']) && is_array($params['formats']) && sizeof($params['formats']) == 3 ? $params['formats'] : ['d.m.Y', 'd.m.', 'd.'];
+  $fullFormat = $formats[0];
+  $monthFormat = $formats[1];
+  $dayFormat = $formats[2];
 
   if ($sameDay) {
-    return $from->format($fullFormat);
+    $result = $from->format($fullFormat);
   }
   elseif ($sameMonth) {
-    return $from->format($dayFormat)." - ".$to->format($fullFormat);
+    $result = $from->format($dayFormat)." - ".$to->format($fullFormat);
   }
   elseif ($sameYear) {
-    return $from->format($monthFormat)." - ".$to->format($fullFormat);
+    $result = $from->format($monthFormat)." - ".$to->format($fullFormat);
   }
-  return $from->format($fullFormat)." - ".$to->format($fullFormat);
+  else {
+    $result = $from->format($fullFormat)." - ".$to->format($fullFormat);
+  }
+
+  // localize names
+  $message = ObjectFactory::getInstance('message');
+  $names = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday",
+      "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+  ];
+  foreach ($names as $name) {
+    $localizeName = $message->getText($name, null, $lang);
+    $localizeShortName = substr($localizeName, 0, 3);
+    $result = strtr($result, [$name => $localizeName, substr($name, 0, 3) => $localizeShortName]);
+  }
+
+  return $result;
 }
 ?>
---
+++ src/wcmf/application/views/plugins/modifier.localize_date.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * wCMF - wemove Content Management Framework
+ * Copyright (C) 2005-2017 wemove digital solutions GmbH
+ *
+ * Licensed under the terms of the MIT License.
+ *
+ * See the LICENSE file distributed with this work for
+ * additional information.
+ */
+use wcmf\lib\core\ObjectFactory;
+
+/**
+ * Date names to be included by l10n tools
+ * - $message->getText("Monday")
+ * - $message->getText("Tuesday")
+ * - $message->getText("Wednesday")
+ * - $message->getText("Thursday")
+ * - $message->getText("Friday")
+ * - $message->getText("Saturday")
+ * - $message->getText("Sunday")
+ * - $message->getText("January")
+ * - $message->getText("February")
+ * - $message->getText("March")
+ * - $message->getText("April")
+ * - $message->getText("May")
+ * - $message->getText("June")
+ * - $message->getText("July")
+ * - $message->getText("August")
+ * - $message->getText("September")
+ * - $message->getText("October")
+ * - $message->getText("November")
+ * - $message->getText("December")
+ */
+
+/**
+ * Localize the month and day names in a date string
+ *
+ * Example:
+ * @code
+ * {$date|date_format:"F Y"|localize_date:$language}
+ * @endcode
+ *
+ * @param $date The date
+ * @param $lang The language
+ * @return String
+ */
+function smarty_modifier_localize_date($date, $lang) {
+  // localize names
+  $message = ObjectFactory::getInstance('message');
+  $names = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday",
+      "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+  ];
+  foreach ($names as $name) {
+    $localizeName = $message->getText($name, null, $lang);
+    $localizeShortName = substr($localizeName, 0, 3);
+    $date = strtr($date, [$name => $localizeName, substr($name, 0, 3) => $localizeShortName]);
+  }
+  return $date;
+}
+?>
