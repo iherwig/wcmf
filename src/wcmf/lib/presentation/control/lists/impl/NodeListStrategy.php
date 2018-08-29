5b9fc328c82955d0a867a2774c3ec5f881d557f2
iherwig <ingo@wemove.com>
2018-08-29 17:28:19 +0200

    Add caching to NodeListStrategy

--- src/wcmf/lib/presentation/control/lists/impl/NodeListStrategy.php
+++ src/wcmf/lib/presentation/control/lists/impl/NodeListStrategy.php
@@ -10,6 +10,7 @@
  */
 namespace wcmf\lib\presentation\control\lists\impl;
 
+use wcmf\lib\io\Cache;
 use wcmf\lib\config\ConfigurationException;
 use wcmf\lib\core\ObjectFactory;
 use wcmf\lib\model\StringQuery;
@@ -38,6 +39,18 @@ use wcmf\lib\util\StringUtil;
  */
 class NodeListStrategy implements ListStrategy {
 
+  const CACHE_SECTION = 'nodeliststrategy';
+
+  protected $cache = null;
+
+  /**
+   * Constructor
+   * @param $dynamicCache Cache instance
+   */
+  public function __construct(Cache $dynamicCache) {
+    $this->cache = $dynamicCache;
+  }
+
   /**
    * @see ListStrategy::getList
    * $options is an associative array with keys 'types' and 'query' (optional)
@@ -49,22 +62,36 @@ class NodeListStrategy implements ListStrategy {
     $types = $options['types'];
 
     $isSingleType = sizeof($types) == 1;
-    $localization = ObjectFactory::getInstance('localization');
+    $hasQuery = isset($options['query']) && strlen($options['query']) > 0;
+
+    $localization = $language != null ? ObjectFactory::getInstance('localization') : null;
+
+    // set cache id for unfiltered result
+    $cacheId = !$hasQuery ? md5(json_encode($options)) : null;
 
     $list = [];
-    foreach ($types as $type) {
-      $query = new StringQuery($type);
-      if (isset($options['query'])) {
-        $query->setConditionString($options['query']);
+    if ($cacheId && $this->cache->exists(self::CACHE_SECTION, $cacheId)) {
+      $list = $this->cache->get(self::CACHE_SECTION, $cacheId);
+    }
+    else {
+      foreach ($types as $type) {
+        $query = new StringQuery($type);
+        if ($hasQuery) {
+          $query->setConditionString($options['query']);
+        }
+        $objects = $query->execute(BuildDepth::SINGLE);
+        foreach ($objects as $object) {
+          if ($language != null) {
+            $object = $localization->loadTranslation($object, $language);
+          }
+          $id = $isSingleType ? $object->getOID()->getFirstId() : $object->getOID()->__toString();
+          $list[$id] = $object->getDisplayValue();
+        }
       }
-      $objects = $query->execute(BuildDepth::SINGLE);
-      foreach ($objects as $object) {
-        $object = $localization->loadTranslation($object, $language);
-        $id = $isSingleType ? $object->getOID()->getFirstId() : $object->getOID()->__toString();
-        $list[$id] = $object->getDisplayValue();
+      if ($cacheId) {
+        $this->cache->put(self::CACHE_SECTION, $cacheId, $list);
       }
     }
-
     return $list;
   }
 
