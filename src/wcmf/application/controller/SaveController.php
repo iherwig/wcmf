iherwig/wcmf

commit 1730fbb1355671ddf44f76276c93796740ab57d8

Prevent commit in rest controller, if subaction has errors, small improvements when saving entities transmitted via form

src/wcmf/application/controller/SaveController.php
@@ -211,6 +211,7 @@ class SaveController extends Controller {
                   // create a new object, if this is an insert request. set the object id
                   // of the request object for correct assignement in save arrays
                   $curNode = $persistenceFacade->create($curOid->getType(), BuildDepth::SINGLE);
+                  $curNode->setOID($curOid);
                 }
                 else {
                   // load the existing object, if this is a save request in order to merge
@@ -457,7 +458,7 @@ class SaveController extends Controller {
         $persistenceFacade = $this->getPersistenceFacade();
         $type = $persistenceFacade->getSimpleType($oid->getType());
         // check if uploadDir.type is defined in the configuration
-        if ($type && ($dir = $config->getDirectoryValue('uploadDir.'.$type, 'media')) !== false) {
+        if ($type && $config->hasValue('uploadDir.'.$type, 'media') && ($dir = $config->getDirectoryValue('uploadDir.'.$type, 'media')) !== false) {
           $uploadDir = $dir;
         }
         else {

src/wcmf/application/controller/RESTController.php
@@ -243,6 +243,11 @@ class RESTController extends Controller {
     // return object only
     $oidStr = $subResponse->hasValue('oid') ? $subResponse->getValue('oid')->__toString() : null;
     $this->handleSubResponse($subResponse, $oidStr);
+
+    // prevent commit
+    if ($subResponse->hasErrors()) {
+      $this->endTransaction(false);
+    }
   }
 
   /**
@@ -290,6 +295,11 @@ class RESTController extends Controller {
         $oidStr = $subResponse->hasValue('oid') ? $subResponse->getValue('oid')->__toString() : '';
       }
     }
+
+    // prevent commit
+    if ($subResponse->hasErrors()) {
+      $this->endTransaction(false);
+    }
     $this->handleSubResponse($subResponse, $oidStr);
   }
 
@@ -332,6 +342,10 @@ class RESTController extends Controller {
       $subResponse = $this->executeSubAction('update');
     }
 
+    // prevent commit
+    if ($subResponse->hasErrors()) {
+      $this->endTransaction(false);
+    }
     $this->handleSubResponse($subResponse, $oidStr);
   }
 
@@ -387,6 +401,10 @@ class RESTController extends Controller {
       $subResponse->setValue($targetOidStr, $targetObj);
     }
 
+    // prevent commit
+    if ($subResponse->hasErrors()) {
+      $this->endTransaction(false);
+    }
     $this->handleSubResponse($subResponse, $targetOidStr);
   }
 
@@ -403,6 +421,11 @@ class RESTController extends Controller {
     $this->requireTransaction();
     // delegate to DeleteController
     $subResponse = $this->executeSubAction('delete');
+
+    // prevent commit
+    if ($subResponse->hasErrors()) {
+      $this->endTransaction(false);
+    }
     $this->handleSubResponse($subResponse);
   }
 
@@ -427,6 +450,11 @@ class RESTController extends Controller {
 
     // delegate to AssociateController
     $subResponse = $this->executeSubAction('disassociate');
+
+    // prevent commit
+    if ($subResponse->hasErrors()) {
+      $this->endTransaction(false);
+    }
     $this->handleSubResponse($subResponse);
   }
 

src/wcmf/lib/presentation/format/impl/HtmlFormat.php
@@ -87,9 +87,10 @@ class HtmlFormat extends AbstractFormat {
         $oid = ObjectId::parse($oidStr);
         if ($oid) {
           $node = &$this->getNode($oid);
-          $pkNames = $node->getMapper()->getPkNames();
+          $mapper = $node->getMapper();
+          $pkNames = $mapper->getPkNames();
           $valueName = $valueDef['name'];
-          if (!in_array($valueName, $pkNames)) {
+          if ($mapper->hasAttribute($valueName) && !in_array($valueName, $pkNames)) {
             $node->setValue($valueName, $value);
           }
           unset($values[$key]);

commit 5064c2f5514af7dfab7a5e475056e12ef243d66e

Fixed regression from 1730fbb1355671ddf44f76276c93796740ab57d8 (related objects where not persisted in AssociateController)

src/wcmf/application/controller/SaveController.php
@@ -211,7 +211,9 @@ class SaveController extends Controller {
                   // create a new object, if this is an insert request. set the object id
                   // of the request object for correct assignement in save arrays
                   $curNode = $persistenceFacade->create($curOid->getType(), BuildDepth::SINGLE);
+                  $transaction->detach($curNode->getOID());
                   $curNode->setOID($curOid);
+                  $transaction->attach($curNode);
                 }
                 else {
                   // load the existing object, if this is a save request in order to merge