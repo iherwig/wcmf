b0ca93e2c631a562e9c2e8f58f0aaa11c2d54a6b
iherwig <ingo@wemove.com>
2021-02-08 01:15:11 +0100

    Add class name to sort order in ListController, if not specified

--- src/wcmf/application/controller/ListController.php
+++ src/wcmf/application/controller/ListController.php
@@ -110,7 +110,7 @@ class ListController extends Controller {
     $sortArray = null;
     $orderBy = $request->getValue('sortFieldName');
     if (strlen($orderBy) > 0) {
-      $sortArray = [$orderBy." ".$request->getValue('sortDirection')];
+      $sortArray = [(strpos($orderBy, '.') === false ? $className."." : "").$orderBy." ".$request->getValue('sortDirection')];
     }
     // get the objects
     $objects = $this->getObjects($className, $query, $sortArray, $pagingInfo);
