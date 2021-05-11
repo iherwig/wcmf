55f2aac84e662c29e554bf6e4f1680719e7039a9
iherwig <ingo@wemove.com>
2021-05-11 13:27:31 +0200

    Improved mimetype detection

--- src/wcmf/lib/io/FileUtil.php
+++ src/wcmf/lib/io/FileUtil.php
@@ -65,17 +65,26 @@ class FileUtil {
    */
   public static function getMimeType($file) {
     $defaultType = 'application/octet-stream';
-    if (class_exists('\FileInfo')) {
-      // use extension
-      $fileInfo = new finfo(FILEINFO_MIME);
-      $fileType = $fileInfo->file(file_get_contents($file));
+
+    // try to determine from file content, if the file exists
+    if (self::fileExists($file)) {
+      if (class_exists('\FileInfo')) {
+        // use FileInfo extension
+        $fileInfo = new finfo(FILEINFO_MIME);
+        $fileType = $fileInfo->file(file_get_contents($file));
+      }
+      else {
+        // try detect image mime type
+        $imageInfo = @getimagesize($file);
+        $fileType = isset($imageInfo['mime']) ? $imageInfo['mime'] : '';
+      }
+      return (is_string($fileType) && !empty($fileType)) ? $fileType : $defaultType;
     }
-    else {
-      // try detect image mime type
-      $imageInfo = @getimagesize($file);
-      $fileType = isset($imageInfo['mime']) ? $imageInfo['mime'] : '';
-    }
-    return (is_string($fileType) && !empty($fileType)) ? $fileType : $defaultType;
+
+    // fall back to file extension
+    $pieces = preg_split('/\./', self::basename($file));
+    $extension = strtolower(array_pop($pieces));
+    return isset(self::MIME_TYPE_MAP[$extension]) ? self::MIME_TYPE_MAP[$extension] : $defaultType;
   }
 
   /**
@@ -328,5 +337,60 @@ class FileUtil {
     $parts = explode('/', $file);
     return end($parts);
   }
+
+  private const MIME_TYPE_MAP = [
+    'txt' => 'text/plain',
+    'htm' => 'text/html',
+    'html' => 'text/html',
+    'php' => 'text/html',
+    'css' => 'text/css',
+    'js' => 'application/javascript',
+    'json' => 'application/json',
+    'xml' => 'application/xml',
+    'swf' => 'application/x-shockwave-flash',
+    'flv' => 'video/x-flv',
+
+    // images
+    'png' => 'image/png',
+    'jpe' => 'image/jpeg',
+    'jpeg' => 'image/jpeg',
+    'jpg' => 'image/jpeg',
+    'gif' => 'image/gif',
+    'bmp' => 'image/bmp',
+    'ico' => 'image/vnd.microsoft.icon',
+    'tiff' => 'image/tiff',
+    'tif' => 'image/tiff',
+    'svg' => 'image/svg+xml',
+    'svgz' => 'image/svg+xml',
+
+    // archives
+    'zip' => 'application/zip',
+    'rar' => 'application/x-rar-compressed',
+    'exe' => 'application/x-msdownload',
+    'msi' => 'application/x-msdownload',
+    'cab' => 'application/vnd.ms-cab-compressed',
+
+    // audio/video
+    'mp3' => 'audio/mpeg',
+    'qt' => 'video/quicktime',
+    'mov' => 'video/quicktime',
+
+    // adobe
+    'pdf' => 'application/pdf',
+    'psd' => 'image/vnd.adobe.photoshop',
+    'ai' => 'application/postscript',
+    'eps' => 'application/postscript',
+    'ps' => 'application/postscript',
+
+    // ms office
+    'doc' => 'application/msword',
+    'rtf' => 'application/rtf',
+    'xls' => 'application/vnd.ms-excel',
+    'ppt' => 'application/vnd.ms-powerpoint',
+
+    // open office
+    'odt' => 'application/vnd.oasis.opendocument.text',
+    'ods' => 'application/vnd.oasis.opendocument.spreadsheet',
+  ];
 }
 ?>
