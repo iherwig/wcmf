ffaf8d56d129d04d545605931d56b5756582bad6
iherwig <ingo@wemove.com>
2023-05-23 21:31:13 +0000

    Migrate number of lines calculation for pdf documents

--- src/wcmf/lib/pdf/PDF.php
+++ src/wcmf/lib/pdf/PDF.php
@@ -172,47 +172,46 @@ class PDF extends Fpdi {
    * @param $txt The text
    */
   public function NbLines($w, $txt) {
-    $cw=&$this->CurrentFont['cw'];
-    if($w==0) {
-      $w=$this->w-$this->rMargin-$this->x;
+    if ($w==0) {
+      $w = $this->w-$this->rMargin-$this->x;
     }
-    $wmax=($w-2*$this->cMargin)*1000/$this->FontSize;
-    $s=str_replace("\r", '', $txt);
-    $nb=strlen($s);
-    if($nb>0 && $s[$nb-1]=="\n") {
+    $wmax = $w-2*$this->cMargin;
+    $s = str_replace("\r", '', $txt);
+    $nb = strlen($s);
+    if ($nb>0 && $s[$nb-1]=="\n") {
       $nb--;
     }
-    $sep=-1;
-    $i=0;
-    $j=0;
-    $l=0;
-    $nl=1;
-    while($i<$nb) {
-      $c=$s[$i];
-      if($c=="\n") {
+    $sep = -1;
+    $i = 0;
+    $j = 0;
+    $l = 0;
+    $nl = 1;
+    while ($i<$nb) {
+      $c = $s[$i];
+      if ($c=="\n") {
         $i++;
-        $sep=-1;
-        $j=$i;
-        $l=0;
+        $sep = -1;
+        $j = $i;
+        $l = 0;
         $nl++;
         continue;
       }
-      if($c==' ') {
-        $sep=$i;
+      if ($c==' ') {
+        $sep = $i;
       }
-      $l+=isset($cw[$c]) ? intval($cw[$c]) : 0;
-      if($l>$wmax) {
-        if($sep==-1) {
-          if($i==$j) {
+      $l += $this->GetStringWidth($c);
+      if ($l>$wmax) {
+        if ($sep==-1) {
+          if ($i==$j) {
             $i++;
           }
         }
         else {
-          $i=$sep+1;
+          $i = $sep+1;
         }
-        $sep=-1;
-        $j=$i;
-        $l=0;
+        $sep = -1;
+        $j = $i;
+        $l = 0;
         $nl++;
       }
       else {
