1d097ac6f69c6feeda19a51a30dcd18ff23df7e2
iherwig <ingo@wemove.com>
2023-05-24 15:23:44 +0000

    Add transformations for pdf content, fix pdf template usage

--- src/wcmf/lib/pdf/PDF.php
+++ src/wcmf/lib/pdf/PDF.php
@@ -48,16 +48,25 @@ class PDF extends Fpdi {
 
   private $pageStarted = false;
   private $pageEnded = false;
+  private $usedTemplateId = null;
 
   /**
    * Overridden to set the template on the page
-   * TODO check if this is still necessary, if useTemplate is called in PDFTemplate::output() now
    */
   public function Header() {
     parent::Header();
-    if ($this->currentTemplateId) {
-      $this->useTemplate($this->currentTemplateId);
+    if ($this->usedTemplateId) {
+      $this->useTemplate($this->usedTemplateId);
     }
+  }
+
+  /**
+   * Overridden to set the template id
+   * @param $templateId
+   */
+  public function useTemplate($tpl, $x=0, $y=0, $width=null, $height=null, $adjustPageSize=false) {
+    parent::useTemplate($tpl);
+    $this->usedTemplateId = $tpl;
   }
 
   /**
@@ -145,6 +154,106 @@ class PDF extends Fpdi {
    * Close the clipping area created with ClippingRect()
    */
   public function UnsetClipping() {
+    $this->_out('Q');
+  }
+
+  /**
+   * The following code is taken from FPDF Add-On 'Transformations'
+   * @see http://fpdf.de/Addon-79-transformations.html
+   */
+
+  /**
+   * Start a transformation (Scale, Translate, Rotate calls follow)
+   */
+  public function StartTransform() {
+    // save the current graphic state
+    $this->_out('q');
+  }
+
+  /**
+   * Scale the following content
+   * @param $s_x
+   * @param $s_y
+   * @param $x
+   * @param $y
+   */
+  public function Scale($s_x, $s_y, $x='', $y='') {
+    if ($x === '') {
+      $x = $this->x;
+    }
+    if ($y === '') {
+      $y = $this->y;
+    }
+    if ($s_x == 0 || $s_y == 0) {
+      $this->Error('Please use values unequal to zero for Scaling');
+    }
+    $y = ($this->h-$y)*$this->k;
+    $x *= $this->k;
+    // calculate elements of transformation matrix
+    $s_x /= 100;
+    $s_y /= 100;
+    $tm[0] = $s_x;
+    $tm[1] = 0;
+    $tm[2] = 0;
+    $tm[3] = $s_y;
+    $tm[4] = $x*(1-$s_x);
+    $tm[5] = $y*(1-$s_y);
+    // scale the coordinate system
+    $this->Transform($tm);
+  }
+
+  /**
+   * Move the following content
+   * @param $t_x
+   * @param $t_y
+   */
+  public function Translate($t_x, $t_y) {
+    // calculate elements of transformation matrix
+    $tm[0]=1;
+    $tm[1]=0;
+    $tm[2]=0;
+    $tm[3]=1;
+    $tm[4]=$t_x*$this->k;
+    $tm[5]=-$t_y*$this->k;
+    // translate the coordinate system
+    $this->Transform($tm);
+  }
+
+  /**
+   * Rotate the following content
+   * @param $angle
+   * @param $x
+   * @param $y
+   */
+  public function Rotate($angle, $x='', $y='') {
+    if ($x === '') {
+      $x = $this->x;
+    }
+    if ($y === '') {
+      $y = $this->y;
+    }
+    $y = ($this->h-$y)*$this->k;
+    $x *= $this->k;
+    // calculate elements of transformation matrix
+    $tm[0] = cos(deg2rad($angle));
+    $tm[1] = sin(deg2rad($angle));
+    $tm[2] = -$tm[1];
+    $tm[3] = $tm[0];
+    $tm[4] = $x+$tm[1]*$y-$tm[0]*$x;
+    $tm[5] = $y-$tm[0]*$y-$tm[1]*$x;
+    // rotate the coordinate system around ($x,$y)
+    $this->Transform($tm);
+  }
+
+  private function Transform($tm) {
+    $this->_out(sprintf('%.3F %.3F %.3F %.3F %.3F %.3F cm', $tm[0], $tm[1], $tm[2], $tm[3], $tm[4], $tm[5]));
+  }
+
+  /**
+   * End a transformation
+   */
+  function StopTransform() {
+    // restore previous graphic state
     $this->_out('Q');
   }
 
