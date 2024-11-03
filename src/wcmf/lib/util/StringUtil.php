iherwig/wcmf

commit 602ab9e29aaf00752509b56f6823be1faf8461d2

Fix last space calculation in cropString method

src/wcmf/lib/util/StringUtil.php
@@ -157,7 +157,7 @@ class StringUtil {
         if ($node instanceof \DomText) {
           $totalLen += $nodeLen = strlen($node->nodeValue);
           if ($totalLen > $length) {
-            $spacePos = strpos($node->nodeValue, ' ', $nodeLen-($totalLen-$length)-1);
+            $spacePos = strrpos($node->nodeValue, ' ');
             $node->nodeValue = $exact ? substr($node->nodeValue, 0, $nodeLen-($totalLen-$length)) : substr($node->nodeValue, 0, $spacePos);
             // don't add suffix to empty node
             $node->nodeValue .= (strlen($node->nodeValue) > 0 ? $suffix : '');

commit 6711496a2f645f77521d97d85bfca07b992ca7a1

Fix cropString method (regression from 602ab9e29aaf00752509b56f6823be1faf8461d2)

src/wcmf/lib/util/StringUtil.php
@@ -157,7 +157,11 @@ class StringUtil {
         if ($node instanceof \DomText) {
           $totalLen += $nodeLen = strlen($node->nodeValue);
           if ($totalLen > $length) {
-            $spacePos = strrpos($node->nodeValue, ' ');
+            $spacePos = strpos($node->nodeValue, ' ', $nodeLen-($totalLen-$length)-1);
+            if (!$spacePos) {
+              // take last space, if no space is found after offset
+              $spacePos = strrpos($node->nodeValue, ' ');
+            }
             $node->nodeValue = $exact ? substr($node->nodeValue, 0, $nodeLen-($totalLen-$length)) : substr($node->nodeValue, 0, $spacePos);
             // don't add suffix to empty node
             $node->nodeValue .= (strlen($node->nodeValue) > 0 ? $suffix : '');